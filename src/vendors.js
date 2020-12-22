0b0279950f2cedbf4c51afd8d24148f458c26f3e
genshen <genshenchu@gmail.com>
2020-12-22 21:33:36 +0800

    chore: clean unused vue project files

--- src/vendors.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueI18n from 'vue-i18n';
-import ViewUI from 'view-design';
-
-// import { Terminal } from 'xterm';
-import axios from 'axios';
-import Cookies from 'js-cookie';
